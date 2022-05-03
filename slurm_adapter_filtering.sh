022a4523fffb80f11999e24bb99107e3d5cbe65b
thibaultjonckheere <thibault.jonckheere@student.howest.be>
2022-05-03 14:36:58 +0200

    started making a slurm script for adapter filterin

---
+++ slurm_adapter_filtering.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+ 
+#SBATCH -A snic2022-22-434                  # project ID = snic2022-22-434
+#SBATCH -p core                             # core or node
+#SBATCH -n 1                                # number of cores   
+#SBATCH -t 12:00:00                         # max running time
+#SBATCH -J test_adapter_filtering           # job name
+#SBATCH -o test_adapter_filtering%A.out     # standard output
+#SBATCH -e test_adapter_filtering%A.err     # standard error
+#SBATCH --mail-type=ALL                     # notify user of progress
+#SBATCH --mail-user=thibault.jonckheere@student.howest.be
+
+#load modules
+module load bioinfo-tools cutadapt/3.1
+
+#Create job directory
+echo [`date`] Creating job directory
+projDir=`pwd`
+mkdir -p jobs/test_adapter_filtering_$SLURM_JOB_ID
+jobDir=$projDir/jobs/test_adapter_filtering_$SLURM_JOB_ID
+
+#Transfer data to compute node disk
+echo [`date`] Transferring data
+cp data/
+ 
+
+#Run job code
+
+
+#Transfer results back to job directory
+echo [`date`] Receiving result file
+cp
