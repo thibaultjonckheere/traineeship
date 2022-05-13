e054094347e904355f452bbdb9e78ce8f81f91b2
thibaultjonckheere <thibault.jonckheere@student.howest.be>
2022-05-13 11:02:12 +0200

    forgot to save changes to template

--- 220513_slurm_transrate.sh
+++ 220513_slurm_transrate.sh
@@ -2,31 +2,37 @@
  
 #SBATCH -A snic2022-22-434                  # project ID = snic2022-22-434
 #SBATCH -p core                             # core or node
-#SBATCH -n 1                                # number of cores   
-#SBATCH -t 1:00:00                          # max running time
-#SBATCH -J job name                         # job name
-#SBATCH -o out/test_adapter_filtering%A.out # standard output
-#SBATCH -e err/test_adapter_filtering%A.err # standard error
+#SBATCH -n 8                                # number of cores   
+#SBATCH -t 2:00:00                          # max running time
+#SBATCH -J transrate                         # job name
+#SBATCH -o out/129_L001_transrate%A.out # standard output
+#SBATCH -e err/129_L001_transrate%A.err # standard error
 #SBATCH --mail-type=ALL                     # notify user of progress
 #SBATCH --mail-user=thibault.jonckheere@student.howest.be
 
 #load modules
-module load
+module load bioinf-tools transrate/1.0.3
 
 #Create job directory
 echo [`date`] Creating job directory
 projDir=`pwd`
-mkdir -p jobs/'''job_name_'''$SLURM_JOB_ID
-jobDir=$projDir/jobs/'''job_name_'''$SLURM_JOB_ID/
+mkdir -p jobs/129_L001_transrate_$SLURM_JOB_ID
+jobDir=$projDir/jobs/129_L001_transrate_$SLURM_JOB_ID/
 
 #Transfer data to compute node disk
 echo [`date`] Transferring data
-cp data/'''inputfile''' $SNIC_TMP
+cp data/* $SNIC_TMP
 cd $SNIC_TMP
 
 #Run job code
+# $ transrate --assembly transcripts.fa \
+#             --left left.fq \
+#             --right right.fq
+transrate --assembly \
+--left \
+--right
 
 
 #Transfer results back to job directory
 echo [`date`] Receiving result file
-cp '''outputfile''' $jobDir
+cp transrate.csv $jobDir
