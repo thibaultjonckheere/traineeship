d8d65247f36398cfaea844373ab04dca6537122a
thibaultjonckheere <thibault.jonckheere@student.howest.be>
2022-05-23 09:23:53 +0200

    updated to final version used on the Rackham server

--- 220513_slurm_transrate.sh
+++ 220513_slurm_transrate.sh
@@ -4,38 +4,41 @@
 #SBATCH -p core                             # core or node
 #SBATCH -n 8                                # number of cores   
 #SBATCH -t 2:00:00                          # max running time
-#SBATCH -J transrate                         # job name
-#SBATCH -o out/129_L001_transrate%A.out # standard output
-#SBATCH -e err/129_L001_transrate%A.err # standard error
+#SBATCH -J 138_L001_transrate                         # job name
+#SBATCH -o out/138_L001_transrate_%A.out # standard output
+#SBATCH -e err/138_L001_transrate_%A.err # standard error
 #SBATCH --mail-type=ALL                     # notify user of progress
 #SBATCH --mail-user=thibault.jonckheere@student.howest.be
 
 #load modules
-module load bioinf-tools transrate/1.0.3
+module load bioinfo-tools blast/2.9.0+ transrate/1.0.1
 
 #Create job directory
 echo [`date`] Creating job directory
 projDir=`pwd`
-mkdir -p jobs/129_L001_transrate_$SLURM_JOB_ID
-jobDir=$projDir/jobs/129_L001_transrate_$SLURM_JOB_ID/
+mkdir -p jobs/138_L001_transrate_$SLURM_JOB_ID
+jobDir=$projDir/jobs/138_L001_transrate_$SLURM_JOB_ID/
 
 #Transfer data to compute node disk
 echo [`date`] Transferring data
-cp data/* $SNIC_TMP
+cp data/138_L001* data/transcripts_L001.fasta $SNIC_TMP
 cd $SNIC_TMP
 
 #unzipping the fastq files
-gunzip *fastq.gz
+gunzip 138_L001*.fastq.gz
 
 #Run job code
 # $ transrate --assembly transcripts.fa \
 #             --left left.fq \
 #             --right right.fq
-transrate --assembly \
---left \
---right
+transrate --install-deps all
+transrate --assembly transcripts_L001.fasta \
+--left 138_L001_R1_trimmed_paired.fastq \
+--right 138_L001_R2_trimmed_paired.fastq
 
+#removing used files before transfer of results
+rm *fastq *fasta
 
 #Transfer results back to job directory
 echo [`date`] Receiving result file
-cp transrate.csv $jobDir
+cp -r /scratch/$SLURM_JOB_ID/ $jobDir
