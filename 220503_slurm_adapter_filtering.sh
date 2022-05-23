6908dc575dd683de03af9e8084c2b3fa0cacb62b
thibaultjonckheere <thibault.jonckheere@student.howest.be>
2022-05-23 09:16:33 +0200

    final version run on the Rackham server

--- 220503_slurm_adapter_filtering.sh
+++ 220503_slurm_adapter_filtering.sh
@@ -2,31 +2,44 @@
 
 #SBATCH -A snic2022-22-434                  # project ID = snic2022-22-434
 #SBATCH -p core                             # core or node
-#SBATCH -n 1                                # number of cores   
-#SBATCH -t 1:00:00                         # max running time
-#SBATCH -J test_adapter_filtering           # job name
-#SBATCH -o out/test_adapter_filtering%A.out # standard output
-#SBATCH -e err/test_adapter_filtering%A.err # standard error
+#SBATCH -n 4                                # number of cores   
+#SBATCH -t 2:00:00                          # max running time
+#SBATCH -J fastQC_filter_fastQC_138_L001     # job name
+#SBATCH -o out/fastQC_filter_fastQC_138_L001_%A.out # standard output
+#SBATCH -e err/fastQC_filter_fastQC_138_L001_%A.err # standard error
 #SBATCH --mail-type=ALL                     # notify user of progress
 #SBATCH --mail-user=thibault.jonckheere@student.howest.be
 
 #load modules
-module load bioinfo-tools cutadapt/3.1
+module load bioinfo-tools FastQC/0.11.9 cutadapt/3.1
 
 #Create job directory
 echo [`date`] Creating job directory
 projDir=`pwd`
-mkdir -p jobs/test6_adapter_filtering_$SLURM_JOB_ID
-jobDir=$projDir/jobs/test6_adapter_filtering_$SLURM_JOB_ID/
+mkdir -p jobs/fastQC_filter_fastQC_138_L001_$SLURM_JOB_ID
+jobDir=$projDir/jobs/fastQC_filter_fastQC_138_L001_$SLURM_JOB_ID/
 
 #Transfer data to compute node disk
 echo [`date`] Transferring data
-cp data/test6 $SNIC_TMP
+cp data/P18452_138_S51* $SNIC_TMP
 cd $SNIC_TMP
 
-#Run job code
-cutadapt -a TAACTTGGTC...GATTCACGAC -o test6_adapter_filtered.fastq.gz test6
+#Running fastQC=>already happened
+fastqc P18452_138_S51_L001_R1_001.fastq.qz
+fastqc P18452_138_S51_L001_R2_001.fastq.qz
+
+#Run cutadapt for paired adapters
+#cutadapt -a ADAPTER_FWD -A ADAPTER_REV -o out.1.fastq -p out.2.fastq reads.1.fastq reads.2.fastq
+#I use two adapter sequences provided by illumina:
+#Read 1: AGATCGGAAGAGCACACGTCTGAACTCCAGTCA
+#Read 2: AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT
+cutadapt --nextseq-trim=20 -o 138_L001_R1_ca.fastq.gz -p 138_L001_R2_ca.fastq.gz \
+P18452_138_S51_L001_R1_001.fastq.qz P18452_138_S51_L001_R2_001.fastq.qz
+
+#Running fastQC again
+fastqc 138_L001_R1_ca.fastq.gz
+fastqc 138_L001_R2_ca.fastq.gz
 
 #Transfer results back to job directory
 echo [`date`] Receiving result file
-cp test6_adapter_filtered.fastq.gz $jobDir
+cp 138_L001_R{1,2}_ca.fastq.gz *.html $jobDir
