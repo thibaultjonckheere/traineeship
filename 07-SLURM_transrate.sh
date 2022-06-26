6e64425bd7a5dae9ca320747b55b2517445c7496
thibaultjonckheere <thibault.jonckheere@student.howest.be>
2022-06-26 22:25:43 +0200

    final version transrate script

--- 07-SLURM_transrate.sh
+++ 07-SLURM_transrate.sh
@@ -3,10 +3,10 @@
 #SBATCH -A snic2022-22-434                  # project ID = snic2022-22-434
 #SBATCH -p core                             # core or node
 #SBATCH -n 8                                # number of cores   
-#SBATCH -t 2:00:00                          # max running time
-#SBATCH -J 138_L001_transrate                         # job name
-#SBATCH -o out/138_L001_transrate_%A.out # standard output
-#SBATCH -e err/138_L001_transrate_%A.err # standard error
+#SBATCH -t 3:00:00                          # max running time
+#SBATCH -J merged_transrate_trinity                         # job name
+#SBATCH -o out/merged_transrate_trinity_%A.out # standard output
+#SBATCH -e err/merged_transrate_trinity_%A.err # standard error
 #SBATCH --mail-type=ALL                     # notify user of progress
 #SBATCH --mail-user=thibault.jonckheere@student.howest.be
 
@@ -16,25 +16,22 @@ module load bioinfo-tools blast/2.9.0+ transrate/1.0.1
 #Create job directory
 echo [`date`] Creating job directory
 projDir=`pwd`
-mkdir -p jobs/138_L001_transrate_$SLURM_JOB_ID
-jobDir=$projDir/jobs/138_L001_transrate_$SLURM_JOB_ID/
+mkdir -p jobs/merged_transrate_trinity_$SLURM_JOB_ID
+jobDir=$projDir/jobs/merged_transrate_trinity_$SLURM_JOB_ID/
 
 #Transfer data to compute node disk
 echo [`date`] Transferring data
-cp data/138_L001* data/transcripts_L001.fasta $SNIC_TMP
+cp data/* $SNIC_TMP
 cd $SNIC_TMP
-
-#unzipping the fastq files
-gunzip 138_L001*.fastq.gz
 
 #Run job code
 # $ transrate --assembly transcripts.fa \
 #             --left left.fq \
 #             --right right.fq
 transrate --install-deps all
-transrate --assembly transcripts_L001.fasta \
---left 138_L001_R1_trimmed_paired.fastq \
---right 138_L001_R2_trimmed_paired.fastq
+transrate --assembly Trinity.fasta \
+--left R1_normalized_merged.fq.normalized_K25_C50_pctSD200.fq \
+--right R2_normalized_merged.fq.normalized_K25_C50_pctSD200.fq
 
 #removing used files before transfer of results
 rm *fastq *fasta
